1a2d96be66ade5b4123a43812c48ba012bf07293
pratyush <pratyushpandey3897@gmail.com>
2023-02-15 01:27:32 -0700

    typo fix in dataset type

--- src/publish/publish.page.jsx
+++ src/publish/publish.page.jsx
@@ -311,7 +311,7 @@ const PublishPage = () => {
                     <div className="flex justify-center">
                     <label
                             className="form-label mb-1 text-gray-700"
-                        >Dataset Type: </label>
+                        >Asset Type: </label>
                         <div className="form-check form-check-inline">
                             <input onChange={setPublishDetails} className="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="assetType" id="datasetRadio" value="datasetRadio" />
                             <label className="form-check-label inline-block text-gray-800">Dataset</label>
