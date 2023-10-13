698fadfedd3a2670bf3aeb9f0ed30cd45707c576
poojabharathi <poojaoguri@gmail.com>
2023-10-12 23:48:37 -0700

    added user assets(blank) page

--- src/sidebar/sidebar.component.jsx
+++ src/sidebar/sidebar.component.jsx
@@ -6,6 +6,7 @@ const Sidebar = () => {
         <div className="flex flex-col justify-start text-left h-full bg-white shadow-lg rounded-lg items-center pt-5">
             <SidebarItem to="/" text="Marketplace" />
             <SidebarItem to="publish" text="Publish Assets" />
+            <SidebarItem to="userassets" text="User Assets" />
             <SidebarItem to="createCompute" text="New Compute Job" />
             <SidebarItem to="computeStatus" text="Compute Status" />
             <SidebarItem to="fml" text="Federated Learning" />
---
+++ src/userassets/userassets.page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const UserAssetsPage = () => {
+
+
+
+  return (
+    <div className="App">
+      <h1>My Assets</h1>
+    </div>
+  );
+}
+
+export default UserAssetsPage;
